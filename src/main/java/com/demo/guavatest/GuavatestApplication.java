a7dff963555a4613cf35e3431628ceb7d28f8031
superMrl <q55667890@163.com>
2019-01-22 23:38:15 +0800

    demo

---
+++ src/main/java/com/demo/guavatest/GuavatestApplication.java
@@ -0,0 +1,14 @@
+package com.demo.guavatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuavatestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuavatestApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/demo/guavatest/example/string/StringUtil.java
@@ -0,0 +1,10 @@
+package com.demo.guavatest.example.string;
+
+/**
+ * guava操作字符串
+ * Created by supermrl on 2019/1/22.
+ */
+public class StringUtil {
+
+
+}
